5f93a6c2927a14a1e7ba92e6880823c1f86bd76d
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-02-14 16:31:23 +0100

    portals won't teleport you if you pass in from the back

--- Potals v2/Assets/Scripts/Teleportation.cs
+++ Potals v2/Assets/Scripts/Teleportation.cs
@@ -9,32 +9,34 @@ public class Teleportation : MonoBehaviour {
 	bool _player_overlapping = false;
 	Transform _player = null;
 	Rigidbody _player_rb = null;
+	bool _entered_from_back = false;
 
 	void Update () {
 
 		if (!_player_overlapping) return;
 
+		// determin what side of the trigger the player is on
 		Vector3 portal_to_player = _player.position - transform.position;
 		float dot = Vector3.Dot(transform.forward, portal_to_player);
 
-		if (dot < 0f) return;
+		// if player entered from back but has passed to the other side reset the flag
+		if (_entered_from_back && dot < 0) _entered_from_back = false;
+
+		// if player actually passed the teleport trigger. (did not enter from back)
+		if (_entered_from_back ||  dot < 0f) return;
 		
-		// Calculate the rotation between portlas and theaxis to rotate around
+		// Calculate the rotation between portlas and the axis to rotate around
 		float rel_rot = 180 - Vector3.Angle(target.forward, transform.forward);
 		Vector3 normal = Vector3.Cross(target.forward, transform.forward);
 		normal = (normal == Vector3.zero) ? Vector3.up : normal;
 
-		// Rotate the player relative position to the entering portal and
+		// Rotate the players relative position to the entering portal and
 		// move the player to the other portal using the relative position as the offset
 		Quaternion portal_rot_diff = Quaternion.AngleAxis(rel_rot, normal);
 		portal_to_player = portal_rot_diff * portal_to_player;
 		_player.position = target.position + portal_to_player;
 
 		// rotate the player
-
-		// Vector3 new_player_look_dir = portal_rot_diff * _player.forward;
-		// _player.rotation = Quaternion.LookRotation(new_player_look_dir);
-		// _player.rotation = _player.rotation * portal_rot_diff;
 		_player.Rotate(normal, rel_rot);
 
 		// if the player has a rigidbody rotate the velocity
@@ -42,6 +44,7 @@ public class Teleportation : MonoBehaviour {
 			_player_rb.velocity = portal_rot_diff * _player_rb.velocity;
 		}
 
+		// reset flaggs and refrences
 		_player_overlapping = false;
 		_player = null;
 		_player_rb = null;
@@ -54,6 +57,13 @@ public class Teleportation : MonoBehaviour {
 			_player_overlapping = true;
 			_player = other.transform;
 			_player_rb = _player.GetComponent<Rigidbody>();
+
+			// check if player entered from back
+			Vector3 portal_to_player = _player.position - transform.position;
+			float dot = Vector3.Dot(transform.forward, portal_to_player);
+
+			_entered_from_back = (dot > 0f);
+
 		}
 
 	}
