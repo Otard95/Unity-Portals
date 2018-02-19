6f862b21458b4d0a2430b7ed4ea90f7988ce44a7
Otard95 <stian.myklebostad@getmail.no>
2018-02-19 21:49:17 +0100

    PortalManager and PortalGun

---
+++ Potals v2/Assets/Scripts/PortalGun.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+public class PortalGun : MonoBehaviour {
+
+	[SerializeField]
+	Transform playerCamera;
+
+	PortalManager _portal_manager;
+	
+	void Start () {
+		if (playerCamera == null) {
+			Debug.LogError(transform.name + " => PortalGun: Missing playerCamera");
+		}
+		_portal_manager = PortalManager.Instance;
+	}
+	
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1")) {
+			// Shoot the Blue Portal (portalA)
+			RaycastHit hit;
+			if (Physics.Raycast(
+				playerCamera.position,
+				playerCamera.forward,
+				out hit,
+				Int32.MaxValue))
+			{
+
+				_portal_manager.PlaceA(hit);
+
+			}
+
+		}
+
+		if (Input.GetButtonDown("Fire2")) {
+			// Shoot the Blue Portal (portalA)
+			RaycastHit hit;
+			if (Physics.Raycast(
+				playerCamera.position,
+				playerCamera.forward,
+				out hit,
+				Int32.MaxValue)) {
+
+				_portal_manager.PlaceB(hit);
+
+			}
+
+		}
+
+	}
+
+}
---
+++ Potals v2/Assets/Scripts/PortalManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour {
+
+	#region SINGLETON PATTERN
+
+	public static PortalManager _instance;
+	public static PortalManager Instance {
+		get {
+			if (_instance == null) {
+				_instance = GameObject.FindObjectOfType<PortalManager>();
+
+				if (_instance == null) {
+					GameObject container = new GameObject("PortalManager");
+					_instance = container.AddComponent<PortalManager>();
+				}
+			}
+
+			return _instance;
+		}
+	}
+
+	void Awake () {
+		if (portalA == null || portalB == null) {
+			Debug.LogError(transform.name + " => PortalManager: " +
+				"Missing one or more portal references");
+		}
+	}
+
+	#endregion
+
+	#region Unity Fields
+
+	[SerializeField]
+	Transform portalA;
+	[SerializeField]
+	Transform portalB;
+	[SerializeField]
+	float offset = 0.001f;
+
+	#endregion
+
+	Collider _portalableA = null;
+	Collider _portalableB = null;
+
+	public void PlaceA (RaycastHit ray_hit) {
+
+		// place portalA
+		if (ray_hit.collider.CompareTag("Portalable")) {
+			//Fix colliders
+			SwapPortalable(_portalableA, ray_hit.collider);
+			_portalableA = ray_hit.collider;
+			// Get portals new position and roation
+			// Temp fix
+			Vector3 off = ray_hit.transform.up + ray_hit.transform.right / 2;
+			Vector3 position = ray_hit.transform.position + off + (ray_hit.transform.forward * offset);
+			Quaternion rotation = Quaternion.LookRotation(ray_hit.transform.forward, Vector3.up);
+			// Apply position and rotation
+			Place(portalA, position, rotation);
+		}
+
+	}
+
+	public void PlaceB (RaycastHit ray_hit) {
+
+		// place portalA
+		if (ray_hit.collider.CompareTag("Portalable")) {
+			//Fix colliders
+			SwapPortalable(_portalableB, ray_hit.collider);
+			_portalableB = ray_hit.collider;
+			// Get portals new position and roation
+			// Temp fix
+			Vector3 off = ray_hit.transform.up + ray_hit.transform.right / 2;
+			Vector3 position = ray_hit.transform.position + off + (ray_hit.transform.forward * offset);
+			Quaternion rotation = Quaternion.LookRotation(ray_hit.transform.forward, Vector3.up);
+			// Apply position and rotation
+			Place(portalB, position, rotation);
+		}
+
+	}
+
+	void Place (Transform portal, Vector3 position, Quaternion rotation) {
+
+		portal.position = position;
+		portal.rotation = rotation;
+
+	}
+
+	void SwapPortalable (Collider oldPortalable, Collider newPortalable) {
+
+		if (oldPortalable != null) {
+			oldPortalable.GetComponent<Collider>().enabled = true;
+		}
+
+		newPortalable.GetComponent<Collider>().enabled = false;
+
+	}
+
+}
